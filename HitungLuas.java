91542e36e9f50a8b0ff712b6c9958152310f953d
INRELT-DEVELOPMENT <inrelt@gmail.com>
2019-01-25 00:05:27 +0700

    first commit

---
+++ HitungLuas.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class HitungLuas {
+public static void main(String[] args) {
+   int panjang;
+   int lebar;
+   int luas;
+   Scanner scan = new Scanner(System.in);
+   System.out.println("Menghitung Luas Persegi Panjang");
+   System.out.print("Masukan Panjang: ");
+   panjang = scan.nextInt();
+   System.out.print("Masukan Lebar: ");
+   lebar = scan.nextInt();
+   }
+}
