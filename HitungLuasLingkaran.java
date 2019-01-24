a20d971dac3d4c88d0488bfb623c343764f6e7a1
INRELT-DEVELOPMENT <inrelt@gmail.com>
2019-01-25 00:48:35 +0700

    second commit

---
+++ HitungLuasLingkaran.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class HitungLuasLingkaran {
+
+public static int cariLuasPersegiPanjang(int a, int b){
+   int luas;
+   luas = (panjang * lebar);
+   return luas;
+}
+
+public static double cariLuasLingkaran(int jarijari){
+   int luas;
+   double phi = 3.14;
+   double luas,keliling;
+   luas = 0.5 * phi * jarijari * jarijari;
+   return luas;
+}
+
+public static void main(String[] args) {
+   int panjang;
+   int lebar;
+   int luas;
+   Scanner scan = new Scanner(System.in);
+   System.out.println("Menghitung Luas Persegi Panjang");
+   System.out.print("Masukan Panjang: ");
+   panjang = scan.nextInt();
+   System.out.print("Masukan Lebar: ");
+   lebar = scan.nextInt();
+   luas = cariLuasPersegiPanjang(panjang,lebar);
+   System.out.println("Luas persegi Panjang = " +luas);
+   }
+}
